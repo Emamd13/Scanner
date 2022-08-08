be52f5a0a87b999b4a25628814842582c2788d6e
emamd <emanuel.mdutz@gmail.com>
2022-08-08 16:13:03 -0500

    Cambio

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-
+import Constants from 'expo-constants';
 export default function App() {
   const [permisoCamara, setPermisoCamara] = useState(null);
   const [scanned, setScanned] = useState(false);
