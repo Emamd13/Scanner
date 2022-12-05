0e9ecb8747a8996c8e1dc57ee7ffafc465b5f3b8
unknown <jesus@readymind.ms>
2022-12-05 07:40:06 -0600

    Cambios

--- vistas/Registro.js
+++ vistas/Registro.js
@@ -2,7 +2,7 @@
 
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View,ImageBackground } from 'react-native';
+import { StyleSheet, View, ImageBackground, ScrollView } from 'react-native';
 import { Button, Text, Input, Icon, Avatar } from "@rneui/themed";
 import { Rating, AirbnbRating } from 'react-native-ratings';
 
@@ -20,60 +20,62 @@ const Registro = (props) => {
     return (
 
         <>
+            <ScrollView>
+                <Text style={styles.subHeader}>Informacion conductor.</Text>
+                <View style={styles.container}>
+                    <ImageBackground source={require('../assets/Fondo.jpg')} resizeMode="cover" style={styles.container}>
+                        <Avatar size={200} source={{ uri: 'https://cdn.pixabay.com/photo/2019/11/03/20/11/portrait-4599553__340.jpg' }}
+                            rounded></Avatar>
+                        <Text style={styles.fonts} h4>Placa: ALT652L</Text>
+                        <Text style={styles.fonts} h4>Serie: JLT8386LHSJ89S</Text>
+                        <Text style={styles.fonts} h4>Conductor: Maria Luisa Martinez Gonzales</Text>
+                    </ImageBackground>
+                </View>
+                <Text style={styles.subHeader}>Valoracion.</Text>
+                <View style={styles.container}>
+                
+                    <Input
+                        placeholder="Comentarios"
+                        leftIcon={{ type: 'font-awesome', name: 'comment' }}
 
-            <Text style={styles.subHeader}>Informacion conductor.</Text>
-            <View style={styles.container}>
-            <ImageBackground source={require('../assets/Fondo.jpg')} resizeMode="cover" style={styles.container}>
-                <Avatar size={200} source={{ uri: 'https://cdn.pixabay.com/photo/2019/11/03/20/11/portrait-4599553__340.jpg' }}
-                    rounded></Avatar>
-                <Text style={styles.fonts} h4>Placa: ALT652L</Text>
-                <Text style={styles.fonts} h4>Serie: JLT8386LHSJ89S</Text>
-                <Text style={styles.fonts} h4>Conductor: Maria Luisa Martinez Gonzales</Text>
-                </ImageBackground>
-            </View>
-            <Text style={styles.subHeader}>Valoracion.</Text>
-            <View style={styles.container}>
-                <Input
-                    placeholder="Comentarios"
-                    leftIcon={{ type: 'font-awesome', name: 'comment' }}
-
-                />
+                    />
 
 
-        <AirbnbRating
-          count={4}
-          ratingColor='#3498db'
-           ratingBackgroundColor='#c8c7c8'
-          reviews={[
-            'Terrible',
-            'Malo',
-            'Bueno',
-            'Excelente',
-           
-          ]}
-          defaultRating={1}
-          size={40}
-        />
-          <Button
-              title="Registrar"
-              loading={false}
-              loadingProps={{ size: 'small', color: 'white' }}
-              buttonStyle={{
-                backgroundColor: 'rgba(111, 202, 186, 1)',
-                borderRadius: 5,
-              }}
-              titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
-              containerStyle={{
-                marginHorizontal: 50,
-                height: 50,
-                width: 200,
-                marginVertical: 10,
-              }}
-             
-            />
-            
-            </View>
-           
+                    <AirbnbRating
+                        count={4}
+                        ratingColor='#3498db'
+                        ratingBackgroundColor='#c8c7c8'
+                        reviews={[
+                            'Terrible',
+                            'Malo',
+                            'Bueno',
+                            'Excelente',
+
+                        ]}
+                        defaultRating={1}
+                        size={40}
+                    />
+                    <Button
+                        title="Registrar"
+                        loading={false}
+                        loadingProps={{ size: 'small', color: 'white' }}
+                        buttonStyle={{
+                            backgroundColor: 'rgba(111, 202, 186, 1)',
+                            borderRadius: 5,
+                        }}
+                        titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
+                        containerStyle={{
+                            marginHorizontal: 50,
+                            height: 50,
+                            width: 200,
+                            marginVertical: 10,
+                        }}
+
+                    />
+
+                </View>
+            </ScrollView>
+
         </>
     );
 
@@ -93,12 +95,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        flexDirection: 'column'
     },
     image: {
         flex: 1,
         resizeMode: 'cover',
         justifyContent: 'center',
-      },
+    },
     view: {
         flex: 1,
         backgroundColor: '#fff',
